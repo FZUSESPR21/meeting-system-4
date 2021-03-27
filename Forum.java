5e6d759c65cc8992ffa1a3462e52fd7dec1e658b
PengzhenChan <937377257@qq.com>
2021-03-27 15:07:30 +0800

    Create Forum.java

---
+++ Forum.java
@@ -0,0 +1,49 @@
+package pojo;
+
+public class Forum {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecretary() {
+        return secretary;
+    }
+
+    public void setSecretary(String secretary) {
+        this.secretary = secretary;
+    }
+
+    public String getSubChairman() {
+        return subChairman;
+    }
+
+    public void setSubChairman(String subChairman) {
+        this.subChairman = subChairman;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    String name;
+    String secretary;
+    String subChairman;
+    String time;
+    String topic;
+}
